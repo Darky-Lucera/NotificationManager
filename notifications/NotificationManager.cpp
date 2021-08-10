956f8b9969a0732f2a9c1b632be1e9553754a2af
Carlos Aragones <c.aragones@gamehouse.com>
2021-08-10 19:17:14 +0200

    Added license note. Working on MacOS X Mojave (LLVM 10 and LLVM 11)

--- notifications/NotificationManager.cpp
+++ notifications/NotificationManager.cpp
@@ -1,5 +1,12 @@
 #include "NotificationManager.h"
 
+//-----------------------------------------------------------------------------
+// Copyright (C) 2021 Carlos Aragonés
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
+//-----------------------------------------------------------------------------
+
 using namespace MindShake;
 
 NotificationManager::TIDMap     NotificationManager::mTIDNotifications;
--- notifications/NotificationManager.h
+++ notifications/NotificationManager.h
@@ -1,5 +1,12 @@
 #pragma once
 
+//-----------------------------------------------------------------------------
+// Copyright (C) 2021 Carlos Aragonés
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
+//-----------------------------------------------------------------------------
+
 #include <vector>
 #include <unordered_map>
 #include <thread>
